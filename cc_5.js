23ec678dd6cb5204df977eb2d34e1c23d7d0095d
Karim <konanfoi@gmail.com>
2025-02-09 15:41:42 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -10,3 +10,20 @@ let customer = {
 console.log(`Customer Name: ${customer.name}`);
 console.log(`Customer Age: ${customer.age}`);
 console.log(`Customer Email: ${customer.email}`);
+
+//Task 2: Order Details
+
+//Declare an object order
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+    //Add method 
+    displayOrder: function() {
+        console.log(`Order Id: ${this.orderId}`);
+        console.log(`Total Amount: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+};
+order.displayOrder();
