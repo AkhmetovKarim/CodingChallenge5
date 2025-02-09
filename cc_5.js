11134fb32c5d355d7035a901ace34d7649c99eba
Karim <konanfoi@gmail.com>
2025-02-09 15:23:23 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+//Task 1: Customer Profile
+
+//Declare customer object
+let customer = {
+    name: "Jane Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+//Log with template literals
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
